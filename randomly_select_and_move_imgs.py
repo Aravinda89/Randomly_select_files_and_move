07615cd1033a37969e87754c2fb6af6e1289a175
Aravinda <31471559+Aravinda89@users.noreply.github.com>
2019-01-13 13:14:50 +0800

    Add files via upload
    randomly select files and move

---
+++ randomly_select_and_move_imgs.py
@@ -0,0 +1,35 @@
+# Randomly select and move images
+
+import shutil
+import numpy as np
+import os
+
+
+def randomly_select_move(src, dest, percentage=0.1):
+    """
+    Randomly select given #files and move into new destination
+    :param src: Source folder
+    :param dest:Destination folder
+    :param percentage: Testset percentage
+    :return:
+    """
+
+    li = os.listdir(src)
+    num_folders = int(len(li) * percentage)
+    np.random.shuffle(li)
+    dir_list = np.random.choice(li, num_folders)
+    dir_list = list(set(dir_list))
+
+    for f in dir_list:
+        pat = os.path.join(src, f)
+        try:
+            shutil.move(pat, dest)
+        except Exception as e:
+            print("\n" + str(e))
+            print("Error: ", f)
+            continue
+
+
+source = "D:\\Damage_data\\Detection_data\\New folder\\Output\\FrontBumper\\replace - Copy"
+destination = "D:\\Damage_data\\Detection_data\\New folder\\Output\\FrontBumper\\replace_test"
+randomly_select_move(source, destination)
